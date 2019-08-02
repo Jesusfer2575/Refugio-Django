4a5cb3bf61ab753bf073d27ff04faccefe11ac10
Jesusfer2575 <adrian.ipod@hotmail.com>
2019-08-02 13:25:23 -0500

    Se añadió el formulario de creación de rescatistas

--- apps/adopcion/urls.py
+++ apps/adopcion/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import url, include
+
 from . import views
 
+app_name = 'adopcion'
+
+
 urlpatterns = [
-	url(r'^$', views.index)
+	url(r'^$', views.index),
+	url(r'^create', views.create, name='create')
 ]
--- apps/adopcion/views.py
+++ apps/adopcion/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from apps.adopcion.forms import PersonaForm
 
 # Create your views here.
 def index(request):
 	return HttpResponse("Index de Adopción")
+
+def create(request):
+	if request.method == 'POST':
+		form = PersonaForm(request.POST)
+		if form.is_valid():
+			form.save()
+		return redirect('mascotas:index')
+	else:
+		form = PersonaForm()
+
+	return render(request, 'adopcion/create.html', {'form' : form})
